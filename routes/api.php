d6549442d9b9ad7b6c8e6935547faed1bffe692f
osamah <technoabg@gmail.com>
2023-02-22 18:52:13 +0300

    main

--- routes/api.php
+++ routes/api.php
@@ -48,11 +48,17 @@ Route::post(
 
         $ffmpeg = FFMpeg::create([
             'ffmpeg.binaries'  => '/opt/homebrew/bin/ffmpeg',
-            'ffprobe.binaries' => '/opt/homebrew/bin/FFProbe'
+            'ffprobe.binaries' => '/opt/homebrew/bin/FFProbe',
+            // 'ffmpeg.nvenc'     => true, // Enable NVIDIA GPU acceleration
+            // 'ffmpeg.nvenc_device' => '/dev/nvidia0' // Specify the NVIDIA GPU device
+
         ]);
         $ffprobe = FFProbe::create([
             'ffmpeg.binaries'  => '/opt/homebrew/bin/ffmpeg',
-            'ffprobe.binaries' => '/opt/homebrew/bin/FFProbe'
+            'ffprobe.binaries' => '/opt/homebrew/bin/FFProbe',
+            // 'ffmpeg.nvenc'     => false, // Enable NVIDIA GPU acceleration
+            // 'ffmpeg.nvenc_device' => '/dev/nvidia0' // Specify the NVIDIA GPU device
+
         ]);
         $video_extensions = ['mp4', 'mpeg', 'mpeg4', 'mov', 'webm', 'avi'];
 
@@ -78,7 +84,24 @@ Route::post(
                 $bitRate = $ffprobe->streams($file)->videos()->first()->get('bit_rate');
 
                 $format = new X264();
-                $format->setAdditionalParameters(array('-vf', 'zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p'));
+                $format->setAdditionalParameters([
+                    '-vf',
+                    'zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p',
+                    // '-c:v',
+                    // 'h264_nvenc', // Use NVIDIA hardware-accelerated H.264 encoder
+                    // '-gpu',
+                    // '0', // Specify the NVIDIA GPU device
+                    // '-preset',
+                    // 'slow', // Use the slow preset for better quality
+                    // '-rc',
+                    // 'vbr_hq', // Use variable bitrate with high quality
+                    // '-cq',
+                    // '23', // Set the quality level
+                    // '-profile:v',
+                    // 'high', // Use the high profile for compatibility
+                    // '-level',
+                    // '4.1' // Use level 4.1 for compatibility
+                ]);
 
                 if ($bitRate > 500000) {
                     $format->setKiloBitrate(500);
